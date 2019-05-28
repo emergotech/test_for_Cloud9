061ed99e412df86a5b4595d7071dde593c0a4549
khalids226@gmail.com <khalids226@gmail.com>
2019-05-28 05:39:48 +0000

    Initial Commit

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 This is a Test for Emergotech tested by Khalid with Integrate the Production Server.+ 
 <?php
 echo 'Hello, World from Khalid to test the Cloud9 to test with Marwan 123 to 456!!'; ?>
+
+Returns a positive result
